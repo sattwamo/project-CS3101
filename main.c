9dd0a9cb3fabe8ae1af1e676fec6e4501fafac41
Sattwamo Ghosh <sattwamoghosh@gmail.com>
2023-11-10 17:59:01 +0530

    added structures

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+typedef struct flight{
+    char flightID[5];
+    char date[8];
+    char time[5];
+    char source[20];
+    char destination[20];
+    float price[3]; 
+    int availableSeats;
+}FLIGHT;
+
+typedef struct ticket{
+    char ticketNum[5];
+    char flightID[5];
+    char name[30];
+    char priceType;
+    char seatNum[3];
+}TICKET;
+
 int main(){
     printf("Samaaj");
 
